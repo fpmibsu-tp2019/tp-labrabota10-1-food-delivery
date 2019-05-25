90250ad894c08216f0bae727c2865df26a31f66c
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-05-25 16:05:05 +0300

    Add empty XCode iOS project.

---
+++ Food Delivery/Food Delivery/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Food Delivery
+//
+//  Created by Konstantin Tomashevich on 5/25/19.
+//  Copyright © 2019 FoodDeliveryLab10. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
