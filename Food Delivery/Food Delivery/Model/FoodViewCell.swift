bef3a7a13a6d35e379b68d44f5dd83246ba18b36
Dima Furs <dimafurs1337@gmail.ru>
2019-05-30 09:17:54 -0700

    Add FoodViewCell

---
+++ Food Delivery/Food Delivery/Model/FoodViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  FoodViewCell.swift
+//  Food Delivery
+//
+//  Created by dima on 5/30/19.
+//  Copyright © 2019 FoodDeliveryLab10. All rights reserved.
+//
+
+import UIKit
+
+class FoodViewCell : UICollectionViewCell {
+    
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var label: UILabel!
+}
