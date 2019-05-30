f08ee14fe6a32c5438b7b2751bbf5f042dc8a47a
Kirill Zheltovski <kirilzh2000@gmail.com>
2019-05-30 07:15:14 -0700

    Add MapViewController and ResultViewController

---
+++ Food Delivery/Food Delivery/Controller/MapViewController.swift
@@ -0,0 +1,25 @@
+//
+//  MapViewController.swift
+//  Food Delivery
+//
+//  Created by Kirill Zheltovski on 30/05/2019.
+//  Copyright © 2019 FoodDeliveryLab10. All rights reserved.
+//
+
+
+import UIKit
+
+class MapViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
