4895f8e99d3688b78d40d49e9579df391084ea32
Dima Furs <dimafurs1337@gmail.ru>
2019-05-30 09:15:27 -0700

    Add FoodViewController

--- Food Delivery/Food Delivery/Controller/FoodViewController.swift
+++ Food Delivery/Food Delivery/Controller/FoodViewController.swift
@@ -8,17 +8,68 @@
 
 import UIKit
 
-class FoodViewController: UICollectionViewController {
+class FoodViewController : UICollectionViewController {
+    
+    private let foodData = FoodData()
+    private var foodImages: [UIImage] = []
+    
+    var selectedIndexPath: IndexPath? {
+        didSet {
+            print("\(selectedIndexPath)")
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        for foodName in foodData.foodNames {
+            foodImages.append(UIImage(named: foodName)!)
+        }
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return foodData.pListData.count
     }
     
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! FoodViewCell
+        
+        let row = indexPath.row
+        let foodName = foodData.foodNames[row]
+        
+        cell.label.text = foodData.foodNames[indexPath.row] + "\n" + (foodData.pListData[foodName] as! Array)[0]!
+        cell.image.image = foodImages[indexPath.row]
+        
+        return cell
+    }
     
+    /*override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showMoreInfo" {
+            let c = segue.destination as! OrderViewController
+            
+            let row = selectedIndexPath!.row
+            
+            let facultyName = foodData.foodNames[row]
+            
+            /*c.image = facultyImages[row]
+            c.name = facultyName
+            c.date = (facultyData.pListData[facultyName] as! Array)[0]!
+            c.desc = (facultyData.pListData[facultyName] as! Array)[1]!*/
+        }
+    }*/
 }
+/*
+extension FoodViewController {
+    override func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+        if selectedIndexPath == indexPath {
+            selectedIndexPath = nil
+        } else {
+            selectedIndexPath = indexPath
+        }
+        
+        self.performSegue(withIdentifier: "showMoreInfo", sender: self)
+        
+        return false
+    }
+}*/
