d4a0ae0a832908e5c82f1cab7713e9caede1c5a7
Kirill Zheltovski <kirilzh2000@gmail.com>
2019-05-30 07:11:16 -0700

    Add OrderViewController

---
+++ Food Delivery/Food Delivery/Controller/OrderViewController.swift
@@ -0,0 +1,24 @@
+//
+//  OrderViewController.swift
+//  Food Delivery
+//
+//  Created by Kirill Zheltovski on 30/05/2019.
+//  Copyright © 2019 FoodDeliveryLab10. All rights reserved.
+//
+
+import UIKit
+
+class OrderViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
